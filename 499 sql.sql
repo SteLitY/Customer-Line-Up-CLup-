0b91512e901d6a641ca1dd0b8a2922ac68b34a68
David Chen <32561475+SteLitY@users.noreply.github.com>
2020-09-15 13:41:26 -0400

    Update 499 sql.sql

--- 499 sql.sql
+++ 499 sql.sql
@@ -6,12 +6,18 @@ password varchar(32),
 firstName varchar(64),
 middleName varchar(64),
 lastName varchar(64),
-address varchar,
+address varchar(255),
 zipCode varchar(5)
 );
+
+#Testing table above table
+-- insert into customers (password, firstName, middleName, lastName, address, zipCode) VALUES ("1234", "David", "middle", "Chen", "12as" ,"11235");
+-- select * from customers;
+
 
 CREATE TABLE IF NOT EXISTS zipcode(
 zipCode varchar(5),
 city varchar(32),
 state varchar(32),
 );
+
