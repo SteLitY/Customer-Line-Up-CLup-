b2c67549a0075bc706c3e264d6f99319bd768f1f
David Chen <32561475+SteLitY@users.noreply.github.com>
2020-09-15 15:45:07 -0400

    Update 499 sql.sql

--- 499 sql.sql
+++ 499 sql.sql
@@ -1,13 +1,13 @@
 use dc2633; #temporarily using IBM from Hunter. You can use your own db to test for now
 
 CREATE TABLE IF NOT EXISTS customers(
-userID INT AUTO_INCREMENT UNIQUE,
+customerID INT AUTO_INCREMENT UNIQUE,
 password varchar(32),
 firstName varchar(64),
 middleName varchar(64),
 lastName varchar(64),
-address varchar(255),
-zipCode varchar(5)
+email varchar(32),
+cellnumber varchar(12)
 );
 
 #Testing table above table
@@ -15,9 +15,18 @@ zipCode varchar(5)
 -- select * from customers;
 
 
+CREATE TABLE IF NOT EXISTS company(
+companyID INT AUTO_INCREMENT UNIQUE,
+companyName varchar(32)
+
+
+);
+
+
 CREATE TABLE IF NOT EXISTS zipcode(
 zipCode varchar(5),
 city varchar(32),
 state varchar(32),
 );
+
 
