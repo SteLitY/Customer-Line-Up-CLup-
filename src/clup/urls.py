aeb49358580b367cb9d86a8535e0a7622f2e5f7c
KaseyHarvey98 <42356469+KaseyHarvey98@users.noreply.github.com>
2020-09-28 19:32:41 -0400

    Updated urls.py

--- src/clup/urls.py
+++ src/clup/urls.py
@@ -1,11 +1,20 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
-from posts.views import post_list_view
+from posts.views import *
 
 
 urlpatterns = [
+    path('', home_page_view),
+    path('home/', home_page_view),
     path('admin/', admin.site.urls),
-    path('posts/', post_list_view)
-    path('/', home_page)
-]
+    path('contact/', contact_page_view),
+    path('aboutus/', about_us_page_view),
+    path('signin/', signup_signin_page_view),
+    path('signup/', customer_signup_view),
+    path('businesslogin/',business_login_view),
+    path('businesssignup/',business_signup_view),
+    path('reset/', forgot_password_view)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
