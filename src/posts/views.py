73f9cd352cff413fa52725af3e573877605fff37
David Chen <32561475+SteLitY@users.noreply.github.com>
2020-10-16 19:26:57 -0400

    removed duplicate about us

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,112 +1,55 @@
-from django.http import HttpResponse
-from .models import Post
-from .forms import CustomerSignUpForm
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth import login, logout, authenticate
-from django.shortcuts import render, redirect, get_object_or_404, HttpResponseRedirect
-from django.contrib.sites.shortcuts import get_current_site
-from django.utils.encoding import force_text
-from django.contrib.auth.models import User
-from django.db import IntegrityError
-from django.utils.http import urlsafe_base64_decode
-from django.utils.encoding import force_bytes
-from django.utils.http import urlsafe_base64_encode
-from django.template.loader import render_to_string
+{% extends 'base.html' %}
 
-from posts.forms import CustomerSignUpForm
+{% block content %}
+<style>
+img {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
+h2 {
+ text-align:center
+}
+p {
+ text-align:center
+}
+</style>
+<div class="container-fluid home-main">
+    <!-- Sign up form -->
+    <form method="post">
+    	     
 
-# Create your views here.
 
-#CRUD Create Retrieve Update Delete
+        <h1 class="card-title"> Welcome <span class="blinker">!</span></h1>
+        <div class="row">
+                    <div class="card-body">
+                    	{% csrf_token %} 
+                        <div class="form-group">
 
-#List all the posts 
+                            <label for="username" class="col-form-label">Username</label>
+                            <input type="input" class="form-control" name="username" id="inputEmail" placeholder="Login Name" required>
+                            <div class="login-feedback">
+                            
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="password" class="col-form-label">Password</label>
+                            <input type="password" class="form-control" name="password" placeholder="Type your password" required>
+                            <div class="password-feedback">
+                            
+                            </div>
+                        </div>
+                        </div>
 
-def base_view(request, *args, **kwargs):
-    return render(request, "base.html", {})
+        </div>
+        <div style="margin-top: 1em;">
+            <button button type="submit" type="button" class="btn btn-default">Login</button>
+        </div>
+        <br>
+        <p><a href="/reset">Forgot password ?  </a>  | 
+         <a href="/signup">Sign up for Line Up</a>  |
+         <a href="/business_sign_in">Business</a></p>  
+        </form>
+</div>
+{% endblock %}
 
-def home_page_view(request,*args, **kwargs):
-    print(request)
-    print(request.user)
-    return render(request, "home_page.html", {})
-
-def contact_page_view(request, *args, **kwargs):
-
-    return render(request, "contact_us.html", {})
-
-def about_us_page_view(request,*args, **kwargs):
-    return render(request, "about_us.html", {})
-
-def about_us_page_view(request,*args, **kwargs):
-    return render(request, "about_us.html", {})
-
-def signup_signin_page_view(request, *args, **kwargs):
-    if request.POST:
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            if user.is_active:
-                login(request, user)
-                return redirect(home_page_view)
-
-    return render(request, "signin.html", {})
-
-def signout_page_view(request):
-    if request.method == "POST":
-        logout(request)
-        return render(request, "home_page.html", {})
-
-def customer_signup_view(request, *args, **kwargs):
-    if request.method == 'POST':
-        form = CustomerSignUpForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            user.refresh_from_db()  # load the profile instance created by the signal
-            user.profile.middle_name = form.cleaned_data.get('middle_name')
-            user.profile.cell_number = form.cleaned_data.get('cell_number')
-            user.save()
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=user.username, password=raw_password)
-            login(request, user)
-            return render(request, "home_page.html", {})
-        else: 
-            print("here")
-            print(form.errors)  
-    else:
-        form = CustomerSignUpForm()
-    return render(request, "signup.html", {'form': form})
-
-def business_signup_view(request, *args, **kwargs):
-    if request.method == 'POST':
-        form = BusinessSignUpForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            user.refresh_from_db()
-            user.Business1.store_name = form.cleaned_data.get('store_name')
-            user.Business1.store_number = form.cleaned_data.get('store_number')
-            user.Business1.store_address = form.cleaned_data.get('store_address')
-            user.Business1.city = form.cleaned_data.get('city')
-            user.Business1.state = form.cleaned_data.get('state')
-            user.Business1.zipcode = form.cleaned_data.get('zipcode')
-            user.Business1.input_sex = form.cleaned_data.get('input_sex')
-            user.save()
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=user.username, password=raw_password)
-            login(request, user)
-            return redirect(home_page_view)
-        else:
-            form = BusinessSignUpForm()
-        return render(request, 'b_signup.html', {'form': form})
-    return render(request, "b_signup.html", {})
-
-def business_login_view(request, *args, **kwargs):
-	return render(request, "b_login.html", {})
-
-def forgot_password_view(request, *args, **kwargs):
-	return render(request, "reset.html", {})
-
-def control_panel_view(request, *args, **kwargs):
-    return render(request, "control_panel.html", {})
-
-def profile_setting_view(request, *args, **kwargs):
-    return render(request, "profile_setting.html", {})
