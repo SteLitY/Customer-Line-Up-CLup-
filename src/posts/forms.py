8970ad32423476c950241e039a8d05ca2b04f4d6
David Chen <32561475+SteLitY@users.noreply.github.com>
2020-10-16 19:18:28 -0400

    included middle name

--- src/posts/forms.py
+++ src/posts/forms.py
@@ -3,7 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class CustomerSignUpForm(UserCreationForm):
-    # middle_name = forms.CharField(max_length=100, help_text='')
+    middle_name = forms.CharField(max_length=100, help_text='')
     cell_number = forms.CharField(max_length=12, help_text='')
 
     class Meta:
