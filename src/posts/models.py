6f82b81294a22a728a2cf9bf1a5618ba2cd11bee
David Chen <32561475+SteLitY@users.noreply.github.com>
2020-10-16 18:53:24 -0400

    duplicate functions
    two create_user_profile and save_userprofile functions

--- src/posts/models.py
+++ src/posts/models.py
@@ -1,11 +1,4 @@
 from django.db import models
-
-# Create your models here.
-
-class Test(models.Model):
-    title = models.CharField(max_length=120)
-    description = models.TextField()
-
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
@@ -33,6 +26,26 @@ def create_user_profile(sender, instance, created, **kwargs):
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
 
+#Customer Registration
+class Customer(models.Model):
+    first_name = models.CharField(max_length=30, default='')
+    last_name = models.CharField(max_length=30, default='')
+    email =  models.CharField(max_length=127, default='123@aol.com')
+    password = models.CharField(max_length=10, default='')
+
+    def __unicode__(self):
+        return u'%s %s %s %s' % (self.first_name, self.last_name, self.email, self.password)
+
+#business table
+class business(models.Model):
+    email = models.CharField(max_length=127, default='')
+    first_name = models.CharField(max_length=127, default='')
+    middle_name = models.CharField(max_length=127, default='')
+    last_name = models.CharField(max_length=127, default='')
+    password = models.CharField(max_length=127, default='')
+    cellnumber = models.CharField(max_length=127, default='')
+
+    
 #business table
 class Business1(models.Model):
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
@@ -47,11 +60,12 @@ class Business1(models.Model):
 
 
 @receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
+def create_user_profile1(sender, instance, created, **kwargs):
     if created:
         Business1.objects.create(user=instance)
 
 
 @receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
+def save_user_profile1(sender, instance, **kwargs):
     instance.profile.save()
+
