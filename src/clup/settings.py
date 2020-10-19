715e1671368bcc27275f55bec0051e0e62587d40
David Chen <32561475+SteLitY@users.noreply.github.com>
2020-10-19 06:14:23 -0400

    left commit for AWS access key and secret access key

--- src/clup/settings.py
+++ src/clup/settings.py
@@ -147,4 +147,6 @@ STATICFILES_DIRS = [
 EMAIL_BACKEND = 'django_ses.SESBackend'
 
 AWS_SES_REGION_NAME = 'us-east-1' #(ex: us-east-2)
+AWS_ACCESS_KEY_ID = '' #cannot be posted on github. ask David for this info
+AWS_SECRET_ACCESS_KEY = '' #cannot be posted on github. ask David for this info
 AWS_SES_REGION_ENDPOINT ='email.us-east-1.amazonaws.com' #(ex: email.us-east-2.amazonaws.com)
