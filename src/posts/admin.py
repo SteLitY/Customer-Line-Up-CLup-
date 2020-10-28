99ea579a45055bb9b543b0e42d5c27f310adc079
David Chen <32561475+SteLitY@users.noreply.github.com>
2020-10-28 06:27:27 -0400

    someone reverted my work....

--- src/posts/admin.py
+++ src/posts/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Post
+from .models import *
 
-admin.site.register(Post) 
+admin.site.register(Business)
+admin.site.register(Profile) 
+admin.site.register(Customer)
