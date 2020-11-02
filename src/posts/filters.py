9beee4e3515a2cb1fc71831ee572c37dfafb93f7
David Chen <32561475+SteLitY@users.noreply.github.com>
2020-11-02 01:23:51 -0500

    search filter for store_name

--- src/posts/filters.py
+++ src/posts/filters.py
@@ -1,8 +1,10 @@
 import django_filters
 
+from django_filters import CharFilter
 from .models import *
 
 class business_search_filter(django_filters.FilterSet):
+    store_name = CharFilter(field_name='store_name', lookup_expr='icontains')
     class Meta:
         model = Business
-        fields = '__all__'
+        fields = ['store_name']
