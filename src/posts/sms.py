eed4742725c4b593f33887b27bd6c266d3fe4891
Christos Kamaris <55633032+ChristosK97@users.noreply.github.com>
2020-11-13 14:51:10 -0500

    Added file for sms api

---
+++ src/posts/sms.py
@@ -0,0 +1,18 @@
+import boto3
+import os
+
+
+client = boto3.client(
+    "sns",
+    aws_access_key_id= '',
+    aws_secret_access_key= '',
+    region_name= "us-east-2"
+)
+def sendtext(userphone, usersname):
+    userphonenumber = '1' + userphone
+    client.publish(
+        PhoneNumber=userphonenumber,
+        Message="Thanks for joining Clup " + usersname + "!"
+ )
+
+
