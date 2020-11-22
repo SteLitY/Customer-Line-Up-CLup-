31b3a381c8f420246021413af9ed7ee0682a3692
Ethan Sam <esam5556@gmail.com>
2020-11-22 16:46:11 -0500

    Updated settings

--- src/clup/settings.py
+++ src/clup/settings.py
@@ -21,14 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'i$9&j7#s)+xpi&kx#i5=bpo@sa#da@dfteoy=pmf$%z-2zi^ou'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','customerlineup.herokuapp.com']
 
 
 # Application definition
