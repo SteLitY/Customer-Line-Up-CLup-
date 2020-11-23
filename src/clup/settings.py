36f5480552757cdbf6d11dc16e31e88d0ba77445
Ethan Sam <esam5556@gmail.com>
2020-11-22 20:02:00 -0500

    Returned secret key to settings.py

--- src/clup/settings.py
+++ src/clup/settings.py
@@ -22,8 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
-
+# SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = "052ff5447f025dfdafc2124544768ed6e7e477796f97f2b4"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
