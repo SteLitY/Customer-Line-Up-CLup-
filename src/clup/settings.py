0f17db857038e5968db07f48dcb85bc256c1cea6
Ethan Sam <esam5556@gmail.com>
2020-12-01 14:42:21 -0500

    Updated Settings.py

--- src/clup/settings.py
+++ src/clup/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = os.environ.get('SECRET_KEY')
-SECRET_KEY = "052ff5447f025dfdafc2124544768ed6e7e477796f97f2b4"
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
